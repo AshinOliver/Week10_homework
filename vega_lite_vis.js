006ae6f6bded940865e6b427b8ec1ce274a3fa1d
AshinOliver <89186258+AshinOliver@users.noreply.github.com>
2021-10-06 11:29:44 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "dup1.json";
+vegaEmbed("#map1", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Week10scatter.json";
+vegaEmbed("#map2", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
